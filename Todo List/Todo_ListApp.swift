d4efa9cc837bea3f740dd38b77808c97d3fd5f2c
raihanArman <raihanarman8@gmail.com>
2023-08-01 20:29:20 +0700

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Raihan Arman on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
